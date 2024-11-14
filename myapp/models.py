6919b2a52ffbbc5b3a6cd716b0a2141775b96d4d
monyango21@student.kibu.ac.ke <monyango21@student.kibu.ac.ke>
2024-11-14 11:28:28 +0300

    working with model

--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,14 @@
 from django.db import models
 
 # Create your models here.
-class Contact(models.Model):
-    name = models.CharField(max_length=100)
-    email = models.EmailField(max_length=254)
-    subject = models.CharField(max_length=255)
-    message = models.TextField()
-    
-    #to return the value in human readable format
+class Booking(models.Model):
+    name = models.CharField(max_length=255)  # Name of the person making the reservation
+    email = models.EmailField(max_length=254)  # Email address of the person
+    phone = models.CharField(max_length=20)  # Phone number of the person
+    date = models.DateField()  # Reservation date
+    time = models.TimeField()  # Reservation time
+    people = models.PositiveIntegerField()  # Number of people for the reservation
+    message = models.TextField(blank=True)  # Additional message from the user (optional)
     
     def __str__(self):
         return self.name
